2ac8a0cd9f07fd1c6beab35e964bf90026f95811
mksigrest <mksigrest@gmail.com>
2025-10-02 08:35:18 -0400

    moved .cs file
    moved raw csharp file to new logic-and loops file

--- logic-and-loops/logic-and-loops/Program.cs
+++ logic-and-loops/logic-and-loops/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Variables
+            string playerName = "adventurer";
+
+            
+
+
+            //Program Start
+            Console.Write("Welcome to the jungle\nWe got fun and games\nWe got everything you want\nHoney, we know the names\n\n");
+            Console.WriteLine("Wait... I don't actually know your name.");
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("[Please enter your name, then press 'ENTER' on the keyboard]");
+
+            playerName = Console.ReadLine();
+            Console.WriteLine("\nWelcome to the jungle " + playerName + "!");
+
+
+            Console.WriteLine("" +
+                "We approach a clearing in the jungle\n" +
+                "There seems to be a trail to the left...\n" +
+                "And a cave entrance to the right.\n"
+            );
+
+            Console.WriteLine("Which path do you chose?");
+            Console.WriteLine("Type 'left' to choose the path and go left or type 'right' to choose the cave and go right.");
+             
+        }
+    }
+}
